89030442a73b01f0a2060bfbce709d9cb8bbfac7
iyaaojik <muhfauhid@gmail.com>
2023-11-14 15:16:25 +0700

    done

---
+++ case1.sql
@@ -0,0 +1,7 @@
+USE jikk_store;
+
+INSERT INTO Transactions (transaction_id, customer_id, product_id, transaction_date, amount, payment_id)
+VALUES
+    (501, 1102452, 110, '2023-11-09', '850.000', '201'),
+    (502, 1102452, 120, '2023-11-11', '90.000', '202'),
+    (503, 1102452, 130, '2023-11-12', '160.000', '204');
